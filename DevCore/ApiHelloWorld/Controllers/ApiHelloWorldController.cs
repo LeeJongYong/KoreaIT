07c0279bf7a5fcd20f5fb5dded12997bfcb6fee5
JongYong <jongyong924@gmail.com>
2022-02-25 09:48:26 +0900

    .net Core 연습
    뷰 컴포넌트 & Web Api 실습

---
+++ DevCore/ApiHelloWorld/Controllers/ApiHelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ApiHelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiHelloWorldController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "안녕", "반가워" };
+        }
+
+        [HttpGet("{id:int}")]
+        public string Get(int id)
+        {
+            return $"넘어온 값 : {id}";
+        }
+    }
+}
---
+++ DevCore/DotNetNote/Controllers/VIewComponentDemoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class VIewComponentDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CopyrightDemo()
+        {
+            return View();
+        }
+
+        public IActionResult TechListDemo() { return View(); }
+    }
+}
